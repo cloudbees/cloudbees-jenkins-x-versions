698b02d4a9d1345a718ba35e4d184e2edcd1e323
Daniel Gozalo <dgozalo@cloudbees.com>
2020-01-13 11:25:01 +0100

    feat: fix dir

--- jx/bdd/boot-eks/report.sh
+++ jx/bdd/boot-eks/report.sh
@@ -13,7 +13,7 @@ DATE=$(date '+%F')
 # activate the GCP service account before uploading the report
 gcloud auth activate-service-account --key-file $GKE_SA
 
-touch reports/bdd_boot_eks_report.html
+touch /workspace/source/reports/bdd_boot_eks_report.html
 
 jx step stash \
     -c tests \
